27a244ced7293fffa38128f29a31cd554b9c28d1
Fabio Tesser <fabio.tesser@fairmat.com>
2024-12-08 19:20:35 +0100

    Added parametrized tests.

--- tests/test_05_collect_dependencies.py
+++ tests/test_05_collect_dependencies.py
@@ -429,11 +429,10 @@ def test_scope_session_collect_dependencies_true_single_test_run_4a(ctestdir):
     """)
 
 
-
-
 @pytest.fixture(scope="session")
 def pytest_order_plugin(request):
     return pytest.importorskip("pytest_order.plugin", reason="This test requires the pytest-order package")
+
 
 def test_scope_session_collect_dependencies_true_single_test_run_4b(ctestdir, pytest_order_plugin):
     """Two modules, some cross module dependencies in session scope.
@@ -508,7 +507,6 @@ def test_scope_session_collect_dependencies_true_single_test_run_4b(ctestdir, py
     """)
 
 
-
 def test_collect_dependencies_true_single_class(ctestdir):
     """A pytest.ini is present, collect_dependencies is set to true.
 
@@ -575,6 +573,7 @@ def test_collect_dependencies_true_different_classes(ctestdir):
         test_scope_session_01.py::TestOtherTests::test_d PASSED
     """)
 
+
 def test_collect_dependencies_named(ctestdir):
     """A pytest.ini is present, collect_dependencies is set to true.
 
@@ -607,7 +606,7 @@ def test_collect_dependencies_named(ctestdir):
             pass
     """)
     result = ctestdir.runpytest("--verbose", "test_collect_dependencies_named.py::test_d")
-    result.assert_outcomes(passed=1,skipped=2, failed=1)
+    result.assert_outcomes(passed=1, skipped=2, failed=1)
     result.stdout.re_match_lines(r"""
         .*::test_a PASSED
         .*::test_b FAILED
@@ -616,4 +615,142 @@ def test_collect_dependencies_named(ctestdir):
     """)
 
 
-# TODO add parameterized tests
+def test_collect_simple_params(ctestdir, pytest_order_plugin):
+    """A pytest.ini is present, collect_dependencies is set to true.
+
+    Explicitly select only a single test that depends on another one using mark.dependency name attribute.
+    Since collect_dependencies is set to true, the other test will be collected, and all tests will be run.
+
+    Simple test for a dependency on a parametrized test.
+    pytest-order is required to reorder the tests in the parametrized cases.
+    """
+    ctestdir.makefile('.ini', pytest="""
+                    [pytest]
+                    collect_dependencies = true
+                    console_output_style = classic
+                """)
+
+    ctestdir.makepyfile("""
+        import pytest
+
+        _md = pytest.mark.dependency
+
+        @pytest.mark.parametrize("x", [ 0, 1 ])
+        @pytest.mark.dependency()
+        def test_a(x):
+            assert x == 0
+
+        @pytest.mark.parametrize("x", [
+            pytest.param(0, marks=_md(depends=["test_a[0]"])),
+            pytest.param(1, marks=_md(depends=["test_a[1]"])),
+        ])
+        def test_b(x):
+            pass
+    """)
+    result = ctestdir.runpytest("--verbose", "test_collect_simple_params.py::test_b", "--order-dependencies")
+    result.assert_outcomes(passed=2, skipped=1, failed=1)
+    # match the following output lines without take into account the order of the tests
+    result.stdout.re_match_lines(r"""
+        .*::test_a\[0\] PASSED
+    """)
+    result.stdout.re_match_lines(r"""
+        .*::test_a\[1\] FAILED
+    """)
+    result.stdout.re_match_lines(r"""
+        .*::test_b\[0\] PASSED
+    """)
+    result.stdout.re_match_lines(r"""
+        .*::test_b\[1\] SKIPPED(?:\s+\(.*\))?
+    """)
+
+
+def test_collect_multiple_param(ctestdir, pytest_order_plugin):
+    """A pytest.ini is present, collect_dependencies is set to true.
+
+    Explicitly select only a single test that depends on another one using mark.dependency name attribute.
+    Since collect_dependencies is set to true, the other test will be collected, and all tests will be run.
+
+    A scenario featuring parametrized tests.
+    pytest-order is required to reorder the tests in the parametrized cases.
+    """
+    ctestdir.makefile('.ini', pytest="""
+                        [pytest]
+                        collect_dependencies = true
+                        console_output_style = classic
+                    """)
+
+    ctestdir.makepyfile("""
+        import pytest
+
+        _md = pytest.mark.dependency
+
+        @pytest.mark.parametrize("x,y", [
+            pytest.param(0, 0, marks=_md(name="a1")),
+            pytest.param(0, 1, marks=_md(name="a2")),
+            pytest.param(1, 0, marks=_md(name="a3")),
+            pytest.param(1, 1, marks=_md(name="a4"))
+        ])
+        def test_a(x,y):
+            assert x==0 or y==0
+
+        @pytest.mark.parametrize("u,v", [
+            pytest.param(1, 2, marks=_md(name="b1", depends=["a1", "a2"])),
+            pytest.param(1, 3, marks=_md(name="b2", depends=["a1", "a3"])),
+            pytest.param(1, 4, marks=_md(name="b3", depends=["a1", "a4"])),
+            pytest.param(2, 3, marks=_md(name="b4", depends=["a2", "a3"])),
+            pytest.param(2, 4, marks=_md(name="b5", depends=["a2", "a4"])),
+            pytest.param(3, 4, marks=_md(name="b6", depends=["a3", "a4"]))
+        ])
+        def test_b(u,v):
+            pass
+
+        @pytest.mark.parametrize("w", [
+            pytest.param(1, marks=_md(name="c1", depends=["b1", "b3", "b5"])),
+            pytest.param(2, marks=_md(name="c2", depends=["b1", "b3", "b6"])),
+            pytest.param(3, marks=_md(name="c3", depends=["b1", "b2", "b4"]))
+        ])
+        def test_c(w):
+            pass
+    """)
+    result = ctestdir.runpytest("--verbose", "test_collect_multiple_param.py::test_c", "--order-dependencies")
+    result.assert_outcomes(passed=7, skipped=5, failed=1)
+    # match the following output lines without take into account the order of the tests
+    result.stdout.re_match_lines(r"""
+        .*::test_a\[0-0\] PASSED
+    """)
+    result.stdout.re_match_lines(r"""
+        .*::test_a\[0-1\] PASSED
+    """)
+    result.stdout.re_match_lines(r"""
+        .*::test_a\[1-0\] PASSED
+    """)
+    result.stdout.re_match_lines(r"""
+        .*::test_a\[1-1\] FAILED
+    """)
+    result.stdout.re_match_lines(r"""
+        .*::test_b\[1-2\] PASSED
+    """)
+    result.stdout.re_match_lines(r"""
+        .*::test_b\[1-3\] PASSED
+    """)
+    result.stdout.re_match_lines(r"""
+        .*::test_b\[1-4\] SKIPPED(?:\s+\(.*\))?
+    """)
+    result.stdout.re_match_lines(r"""
+        .*::test_b\[2-3\] PASSED
+    """)
+    result.stdout.re_match_lines(r"""
+        .*::test_b\[2-4\] SKIPPED(?:\s+\(.*\))?
+    """)
+    result.stdout.re_match_lines(r"""
+        .*::test_b\[3-4\] SKIPPED(?:\s+\(.*\))?
+    """)
+    result.stdout.re_match_lines(r"""
+        .*::test_c\[1\] SKIPPED(?:\s+\(.*\))?
+    """)
+    result.stdout.re_match_lines(r"""
+        .*::test_c\[2\] SKIPPED(?:\s+\(.*\))?
+    """)
+    result.stdout.re_match_lines(r"""
+        .*::test_c\[3\] PASSED
+    """)
